b12fd2043cf355702ad5f11b45db5fc81954ecc6
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-10-18 17:17:12 -0300

    Add docstring for Animal

--- models/animal.py
+++ models/animal.py
@@ -5,6 +5,24 @@ from enums.animal import AnimalType
 
 
 class Animal(BaseModel):
+    """
+        Represents an animal in a database.
+
+        Attributes:
+            - name (str): The name of the animal.
+            - owner_id (int): The ID of the owner (foreign key).
+            - birth_date (date): The date of birth of the animal.
+            - origin (str): The origin or place of birth of the animal.
+            - animal_type (AnimalType): The type of the animal (e.g., 'Cow', 'Bull', 'Heifer', 'Calf').
+
+        Relationships:
+            - kinship (list of Animal): Children of this animal (back reference to Kinship model).
+            - mothership (list of Animal): Animals that are mothers of this animal (back reference to Kinship model).
+            - fathership (list of Animal): Animals that are fathers of this animal (back reference to Kinship model).
+
+        This model represents animals in a database, with fields for their name, owner, birth date, origin, and type.
+        The relationships to the Kinship model define parent-child relationships.
+        """
     name = db.Column(db.String(50), nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     birth_date = db.Column(db.Date)
