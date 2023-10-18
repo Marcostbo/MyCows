364a67826c8a0d90daf1a64b9ab641efd56381fc
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-10-18 15:49:35 -0300

    Rename to Animal: Fix

--- routes/cow.py
+++ routes/cow.py
@@ -1,33 +1,27 @@
-import os
-import uuid
-from datetime import datetime, timedelta
-
-import jwt
-from flask import Blueprint, jsonify, make_response, request
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint, jsonify, request
 
 from database import db
 from decorators.authentication import token_required
-from models import User, Cow
+from models import User, Animal
 
 cows_bp = Blueprint('cows', __name__)
 
 
-@cows_bp.route('/get-cows', methods=['GET'])
+@cows_bp.route('/get-animals', methods=['GET'])
 @token_required
-def get_all_cows(public_id):
+def get_all_animals(public_id):
     current_user = User.query.filter_by(public_id=public_id).first()
-    cows = Cow.query.filter_by(owner=current_user)
+    cows = Animal.query.filter_by(owner=current_user)
     response = [cow.simple_serialize for cow in cows]
     return jsonify({'cows': response})
 
 
-@cows_bp.route('/get-cows/<cow_id>', methods=['GET'])
+@cows_bp.route('/get-animals/<animal_id>', methods=['GET'])
 @token_required
-def get_cow_by_id(public_id, cow_id):
+def get_cow_by_id(public_id, animal_id):
     current_user = User.query.filter_by(public_id=public_id).first()
-    cow = Cow.query.filter_by(owner=current_user).filter_by(id=cow_id).first()
-    return jsonify({'cows': cow.simple_serialize})
+    animal = Animal.query.filter_by(owner=current_user).filter_by(id=animal_id).first()
+    return jsonify({'cows': animal.simple_serialize})
 
 
 @cows_bp.route('/register-cow', methods=['POST'])
@@ -35,13 +29,13 @@ def get_cow_by_id(public_id, cow_id):
 def register_cow(public_id):
     current_user = User.query.filter_by(public_id=public_id).first()
     data = request.form
-    cow_name = data.get('name')
+    animal_name = data.get('name')
 
-    new_cow = Cow(
-        name=cow_name,
+    new_animal = Animal(
+        name=animal_name,
         owner=current_user
     )
     # Register new cow for logged user
-    db.session.add(new_cow)
+    db.session.add(new_animal)
     db.session.commit()
-    return jsonify({'cow': new_cow.simple_serialize})
+    return jsonify({'cow': new_animal.simple_serialize})
