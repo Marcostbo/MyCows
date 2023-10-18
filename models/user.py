911fa7bc2be3c35d0e40485738b0b1ccfe88e311
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-10-18 18:10:01 -0300

    Add docstring

--- models/user.py
+++ models/user.py
@@ -3,6 +3,22 @@ from models.base import BaseModel
 
 
 class User(BaseModel):
+    """
+        Represents a user in the application.
+
+        Attributes:
+            - public_id (str): A unique public UUID4 identifier for the user.
+            - name (str): The name of the user.
+            - email (str): The email address of the user (unique).
+            - password (str): The password of the user.
+            - animals (list of Animal): A list of animals owned by the user.
+
+        This model represents users in the application, with fields for their public ID, name, email, and password.
+        The 'animals' relationship represents the animals owned by the user.
+
+        Relationships:
+            - animals (list of Animal): A list of animals owned by the user (back reference to the Animal model).
+        """
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(70), unique=True)
