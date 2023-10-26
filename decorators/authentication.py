2dd50c6b479dd103dc92e84395818c9b38056f7b
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-10-26 14:29:01 -0300

    Better JWT decorator

--- decorators/authentication.py
+++ decorators/authentication.py
@@ -1,29 +1,25 @@
-import os
 from functools import wraps
-
+from flask import request, jsonify
 import jwt
-from flask import jsonify, request
+import os
 
 
-# Decorator for verifying the JWT
+# Decorator to verify JWT in request
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = None
-        # jwt is passed in the request header
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        # return 401 if token is not passed
+        token = request.headers.get('x-access-token')
+
         if not token:
             return jsonify({'message': 'Token is missing !!'}), 401
-        try:
-            # decoding the payload to fetch the stored details
-            data = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
-        except Exception as err:
-            return jsonify({
-                'message': f'Token is invalid. Error {err.__str__()}'
-            }), 401
-        # returns the current logged user context to the routes
-        return f(data['public_id'], *args, **kwargs)
 
+        try:
+            data = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
+            public_id = data.get('public_id')
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}), 401
+        except jwt.InvalidTokenError as err:
+            return jsonify({'message': f'Token is invalid. Error: {err}'}), 401
+
+        return f(public_id, *args, **kwargs)
     return decorated
