9b15b40b8b511e7c0c7fbc3abe5aedf25a761c52
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-11-10 16:14:35 -0300

    add type hints

--- routes/animal.py
+++ routes/animal.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, make_response, abort
 
-from database import db
 from decorators.authentication import token_required
+from enums.animal import AnimalType
 from models import User, Animal
 from schemas.animal import BaseAnimalSchema, AnimalSchema, CreateAnimalSchema, UpdateAnimalSchema
 from services.animal import AnimalService
@@ -11,7 +11,7 @@ animals_bp = Blueprint('animals', __name__)
 
 @animals_bp.route('/get-animals', methods=['GET'])
 @token_required
-def get_all_animals(public_id):
+def get_all_animals(public_id: str):
     # current_user = User.query.filter_by(public_id=public_id).first()
     # animals = Animal.query.filter_by(owner=current_user)
     animals = Animal.query.join(User).filter_by(public_id=public_id)
@@ -20,8 +20,8 @@ def get_all_animals(public_id):
 
 @animals_bp.route('/get-animals/<animal_id>', methods=['GET'])
 @token_required
-def get_animal_by_id(public_id, animal_id):
-    animal = Animal.query.join(User).filter(User.public_id == public_id, Animal.id == animal_id).first()
+def get_animal_by_id(public_id: str, animal_id: int):
+    animal: Animal = Animal.query.join(User).filter(User.public_id == public_id, Animal.id == animal_id).first()
     if not animal:
         abort(403, 'Permission Denied')
     return jsonify(AnimalSchema().dump(obj=animal))
@@ -29,10 +29,10 @@ def get_animal_by_id(public_id, animal_id):
 
 @animals_bp.route('/update-animal/<animal_id>', methods=['PUT'])
 @token_required
-def update_animal(public_id, animal_id):
+def update_animal(public_id: str, animal_id: int):
     validated_data = UpdateAnimalSchema().load(request.form)
 
-    animal = Animal.query.join(User).filter(User.public_id == public_id, Animal.id == animal_id).first()
+    animal: Animal = Animal.query.join(User).filter(User.public_id == public_id, Animal.id == animal_id).first()
     if not animal:
         abort(403, 'Permission Denied')
 
@@ -44,21 +44,22 @@ def update_animal(public_id, animal_id):
 
 @animals_bp.route('/register-animal', methods=['POST'])
 @token_required
-def register_animal(public_id):
-    current_user = User.query.filter_by(public_id=public_id).first()
+def register_animal(public_id: str):
+    current_user: User = User.query.filter_by(public_id=public_id).first()
     validated_data = CreateAnimalSchema().load(request.form)
 
-    mother = father = None
+    mother: Animal | None = None
+    father: Animal | None = None
     if validated_data.get('mother_id'):
-        mother = Animal.query.filter_by(id=validated_data.get('mother_id')).first()
+        mother: Animal = Animal.query.filter_by(id=validated_data.get('mother_id')).first()
         if not mother:
             return make_response('Invalid ID for animal mother', 400)
     if validated_data.get('father_id'):
-        father = Animal.query.filter_by(id=validated_data.get('father_id')).first()
+        father: Animal = Animal.query.filter_by(id=validated_data.get('father_id')).first()
         if not father:
             return make_response('Invalid ID for animal father', 400)
 
-    animal_type = AnimalService.get_animal_type_by_id(
+    animal_type: AnimalType = AnimalService.get_animal_type_by_id(
         type_id=validated_data.get('animal_type_id')
     )
 
